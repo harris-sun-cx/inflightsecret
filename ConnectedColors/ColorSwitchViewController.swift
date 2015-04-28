04a7010c1f4fef3642408115e5c96907a63210ce
Ralf Ebert <info@ralfebert.de>
2015-04-28 19:23:50 +0200

    Updating the UI

--- ConnectedColors/ColorSwitchViewController.swift
+++ ConnectedColors/ColorSwitchViewController.swift
@@ -16,13 +16,46 @@ class ColorSwitchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorService.delegate = self
     }
 
     @IBAction func redTapped(sender: AnyObject) {
+        self.changeColor(UIColor.redColor())
+        colorService.sendColor("red")
     }
     
     @IBAction func yellowTapped(sender: AnyObject) {
+        self.changeColor(UIColor.yellowColor())
+        colorService.sendColor("yellow")
+    }
+    
+    func changeColor(color : UIColor) {
+        UIView.animateWithDuration(0.2) {
+            self.view.backgroundColor = color
+        }
     }
     
 }
 
+extension ColorSwitchViewController : ColorServiceManagerDelegate {
+    
+    func connectedDevicesChanged(manager: ColorServiceManager, connectedDevices: [String]) {
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+    
+    func colorChanged(manager: ColorServiceManager, colorString: String) {
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            switch colorString {
+            case "red":
+                self.changeColor(UIColor.redColor())
+            case "yellow":
+                self.changeColor(UIColor.yellowColor())
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
+    }
+    
+}
