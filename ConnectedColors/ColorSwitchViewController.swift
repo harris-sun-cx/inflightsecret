ccf076f8d698ccbd279b7130d85cd844f2b11915
Ralf Ebert <info@ralfebert.de>
2015-04-28 18:20:35 +0200

    Project setup

---
+++ ConnectedColors/ColorSwitchViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ConnectedColors
+//
+//  Created by Ralf Ebert on 28/04/15.
+//  Copyright (c) 2015 Ralf Ebert. All rights reserved.
+//
+
+import UIKit
+
+class ColorSwitchViewController: UIViewController {
+
+    @IBOutlet weak var connectionsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func redTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func yellowTapped(sender: AnyObject) {
+    }
+
+}
+
